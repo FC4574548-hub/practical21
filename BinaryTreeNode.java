26fae15b36dea3feac647ba152c511e55189ead9
FC4574548-hub <4574548@myuwc.ac.za>
2026-04-13 14:37:45 +0200

    Added all the get methods

--- BinaryTreeNode.java
+++ BinaryTreeNode.java
@@ -1,1 +1,30 @@
+class BinaryTreeNode<E> {
+    E value;
+    BinaryTreeNode<E> left;
+    BinaryTreeNode<E> right;
+    
+    public BinaryTreeNode(){
+        this.value = null;
+        this.left = null;
+        this.right = null;
+    }
+    
+    public BinaryTreeNode(E value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+    //Getter Methods
+    public E getValue(){
+        return value;
 
+    }
+
+    public BinaryTreeNode<E> getLeft() {
+        return left;
+    }
+
+    public BinaryTreeNode<E> getRight() {
+        return right;
+    }
+}
