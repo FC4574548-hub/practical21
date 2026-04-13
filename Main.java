41021c4979426bc6e6ee0ce76e9de3dee9a2523d
FC4574548-hub <4574548@myuwc.ac.za>
2026-04-13 15:00:11 +0200

    Update Main.java "Added second Binary Tree"

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main(String[] args){
 
-    BinaryTreeNode<Character> a = new BinaryTreeNode<>("a");
-    BinaryTreeNode<Character> b = new BinaryTreeNode<>("b");
-    BinaryTreeNode<Character> c = new BinaryTreeNode<>("c");
-    BinaryTreeNode<Character> d = new BinaryTreeNode<>("d");
-    BinaryTreeNode<Character> e = new BinaryTreeNode<>("e");
+    BinaryTreeNode<Character> a = new BinaryTreeNode<>('a');
+    BinaryTreeNode<Character> b = new BinaryTreeNode<>('b');
+    BinaryTreeNode<Character> c = new BinaryTreeNode<>('c');
+    BinaryTreeNode<Character> d = new BinaryTreeNode<>('d');
+    BinaryTreeNode<Character> e = new BinaryTreeNode<>('e');
 
     a.setLeft(b);
     a.setLeft(c);
@@ -15,5 +15,20 @@ public class Main {
     BinaryTree<Character> bt1 = new BinaryTree<>();
     bt1.setRoot(a);
     bt1.setSize(5);
+
+    BinaryTreeNode<Double> n1 = new BinaryTreeNode<>(3.4);
+    BinaryTreeNode<Double> n2 = new BinaryTreeNode<>(-1.5);
+    BinaryTreeNode<Double> n3 = new BinaryTreeNode<>(2.9);
+    BinaryTreeNode<Double> n4 = new BinaryTreeNode<>(-9.3);
+
+    n1.setLeft(n2);
+    n1.setLeft(n3);
+    n2.setLeft(n4);
+
+    BinaryTree<Double> bt2 = new BinaryTree<>();
+    bt2.setRoot(n1);
+    bt2.setSize(4);
+
+    
   }
 }
