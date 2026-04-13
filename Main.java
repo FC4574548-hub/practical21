3f9655c4c0d8d73abe103d285b4e410ced33a249
FC4574548-hub <4574548@myuwc.ac.za>
2026-04-13 14:50:09 +0200

    Update Main.java "Completed first Binary Tree"

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+public class Main {
+  public static void main(String[] args){
 
+    BinaryTreeNode<Character> a = new BinaryTreeNode<>("a");
+    BinaryTreeNode<Character> b = new BinaryTreeNode<>("b");
+    BinaryTreeNode<Character> c = new BinaryTreeNode<>("c");
+    BinaryTreeNode<Character> d = new BinaryTreeNode<>("d");
+    BinaryTreeNode<Character> e = new BinaryTreeNode<>("e");
+
+    a.setLeft(b);
+    a.setLeft(c);
+    b.setLeft(d);
+    b.setLeft(e);
+
+    BinaryTree<Character> bt1 = new BinaryTree<>();
+    bt1.setRoot(a);
+    bt1.setSize(5);
+  }
+}
